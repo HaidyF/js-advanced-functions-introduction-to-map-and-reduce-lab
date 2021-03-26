1f84acd07e59b57529b71bc84ea84ea01797c738
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-03-25 23:50:06 -0400

    commit tree a06d9878111ac3d00cfbb051bf2b580b62e78f36

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(x => x * -1 )
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(x => x * 2 )
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(x => x * x )
+}
+
+const reduceToTotal = (sourceArray, startingPoint=0) => {
+    let total = startingPoint
+    for(let num of sourceArray){
+        total = total + num 
+    }
+    return total
+}
+
+const reduceToAllTrue = (sourceArray) => {
+    let value = true
+    for(let item of sourceArray) {
+        if (item === false){
+            value = false
+        }
+    }
+    return value
+}
+
+const reduceToAnyTrue = (sourceArray) => {
+    let value = false 
+    for(let item of sourceArray){
+        if (item === true){
+            value = true 
+        }
+    }
+    return value 
+} 
